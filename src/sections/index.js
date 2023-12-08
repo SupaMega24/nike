17427f20b16cd40171da6ba8a122a5f5b506e2ad
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2023-12-08 23:18:14 +0900

    Add sections

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import Services from './Services';
+
+export {
+    Hero, 
+    PopularProducts, 
+    SpecialOffer, 
+    SuperQuality, 
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    Services,
+}
