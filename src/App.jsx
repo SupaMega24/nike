1f027b104acebb33a773f1314a5486152600bd25
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2023-12-09 12:03:01 +0900

    Layout Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import {
   CustomerReviews, Subscribe, Footer, Services,
 } from './sections';
 
+import Nav from './components/Nav';
+
 const App = () => (
   <main className='relative'>
-    {/* Nav */}
+    <Nav />
     <section className='xl:padding-1 wide:padding-r padding-b'>
       <Hero />
     </section>
